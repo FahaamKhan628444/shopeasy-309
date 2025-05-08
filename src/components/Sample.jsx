587f06388c7bc3ee913b2c891d34c6f83cafe1ca
Fahaam Khan <122596213+FahaamKhan628444@users.noreply.github.com>
2025-05-08 10:10:26 +0530

    Add files via upload

---
+++ src/components/Sample.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sample.css';
+
+function Sample() {
+  return (
+    <div className="wrapper">
+      <h1 className="heading">Hello World!</h1>
+      <h4 className="subheading">
+        This is an assignment to deploy a project on Vercel and GitHub.
+      </h4>
+    </div>
+  );
+}
+
+export default Sample;
